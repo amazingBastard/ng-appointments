3d57fac59cc2e354687251a99fe67cde3cc6fdc1
Damir <damir@withpulp.com>
2015-11-18 19:58:25 -0500

    init

---
+++ gulp/copyright.js
@@ -0,0 +1,15 @@
+//        .pipe(plug.header(fs.readFileSync('./gulp/copyright.js'), {version: pkg.version}))
+
+/**
+ * @author Damir Vazgird
+ * @version:
+ *     <%= version %>
+ * @links
+ *     http://github.com/amazingBastard/ng-appointments
+ *     http://twitter.com/dvazgird
+ *     http://damirvazgird.com
+ * @source
+ *     http://github.com/amazingBastard/ng-appointments
+ * @license:
+ *     MIT License http://www.opensource.org/licenses/mit-license.php
+ */
