4c7b976d8e3d04833d2a34181238b4f108a0750b
Damir <damir@withpulp.com>
2015-11-22 11:56:06 -0500

    calendar wip

--- src/client/app/dashboard/dashboard.js
+++ src/client/app/dashboard/dashboard.js
@@ -19,6 +19,57 @@
         vm.avengerCount = 0;
         vm.avengers = [];
         vm.title = 'Appointments';
+        vm.calendarView = 'week';
+        vm.calendarDay = new Date();
+        vm.calendarEvents = [
+            {
+                title: 'An event',
+                type: 'warning',
+                startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+                endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+                draggable: true,
+                resizable: true
+            }, {
+                title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+                type: 'info',
+                startsAt: moment().subtract(1, 'day').toDate(),
+                endsAt: moment().add(5, 'days').toDate(),
+                draggable: true,
+                resizable: true
+            }, {
+                title: 'This is a really long event title that occurs on every year',
+                type: 'important',
+                startsAt: moment().startOf('day').add(7, 'hours').toDate(),
+                endsAt: moment().startOf('day').add(19, 'hours').toDate(),
+                recursOn: 'year',
+                draggable: true,
+                resizable: true
+            }
+        ]
+
+        vm.isCellOpen = false;
+
+        vm.eventClicked = function(event) {
+            showModal('Clicked', event);
+        };
+
+        vm.eventEdited = function(event) {
+            showModal('Edited', event);
+        };
+
+        vm.eventDeleted = function(event) {
+            showModal('Deleted', event);
+        };
+
+        vm.eventTimesChanged = function(event) {
+            showModal('Dropped or resized', event);
+        };
+
+        vm.toggle = function($event, field, event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            event[field] = !event[field];
+        };
 
         activate();
 
@@ -28,6 +79,18 @@
 //            return dataservice.ready(promises).then(function(){
             return $q.all(promises).then(function() {
                 logger.info('Activated Dashboard View');
+            });
+        }
+
+        function showModal(action, event) {
+            $uibModal.open({
+                templateUrl: 'modalContent.html',
+                controller: function() {
+                    var vm = this;
+                    vm.action = action;
+                    vm.event = event;
+                },
+                controllerAs: 'vm'
             });
         }
 
